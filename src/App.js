0a0eb163f6e4b3936c61226c1d3f7fe5b400802e
kushagr14 <klakhanpal14@gmail.com>
2024-04-25 17:25:11 +0530

    plus/minus stars, fav&un-fav btn

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Moviecard from "./Moviecard";
+import MovieList from "./MovieList";
 
 function App() {
   return (
     <>
       <h1>Movie Store</h1>
-      <Moviecard />
+      <MovieList />
     </>
   );
 }
--- src/Moviecard.js
+++ src/Moviecard.js
@@ -3,46 +3,33 @@ import { Component } from "react";
 
 class Moviecard extends Component{
 
-    constructor(){
-        super();
-        this.state={
-            title: "The Avengers",
-            plot: "Supernatural nnvommcmmcmxnwjkdb flsnflsdknc slfnfselncs lseihfklsn",
-            price: 199,
-            rating: 8.9,
-            stars:0,
-            fav: false,
-            cart: false
-        }
-    }
-    countStars=0;
 
-    addStars = () => {
-        // this.setState({
-        //     stars: this.state.stars + 0.5
-        // });
-        if(this.state.stars < 5){
-            this.setState((prevState) => {
-                return {stars: prevState.stars + 0.5}
-            });
-        }
-    }
+    // addStars = () => {
+    //     // this.setState({
+    //     //     stars: this.state.stars + 0.5
+    //     // });
+    //     if(this.state.stars < 5){
+    //         this.setState((prevState) => {
+    //             return {stars: prevState.stars + 0.5}
+    //         });
+    //     }
+    // }
 
-    minusStars = ()=> {
-        if(this.state.stars != 0) {
-            this.setState((prevState) => {
-                return {
-                    stars: prevState.stars - 0.5
-                }
-            });
-        }
-    }
+    // minusStars = ()=> {
+    //     if(this.state.stars !== 0) {
+    //         this.setState((prevState) => {
+    //             return {
+    //                 stars: prevState.stars - 0.5
+    //             }
+    //         });
+    //     }
+    // }
 
-    favHandler = () => {
-        this.setState({
-            fav: !this.state.fav
-        })
-    }
+    // favHandler = () => {
+    //     this.setState({
+    //         fav: !this.state.fav
+    //     })
+    // }
 
     cartHandler = () => {
         this.setState({
@@ -51,13 +38,13 @@ class Moviecard extends Component{
     }
 
     render(){
-        const {title, plot, price, rating, stars} = this.state;
+        const {title, plot, poster,price, rating, stars, fav, cart} = this.props.movie;
 
         return (
             <div className="main">
             <div className="movie-card">
                 <div className="left">
-                <img alt="Poster" src="https://picsum.photos/id/237/400/300"></img>
+                <img alt="Poster" src={poster}></img>
                 </div>
                 <div className="right"> 
                 <div className="title">{title}</div>
@@ -67,18 +54,24 @@ class Moviecard extends Component{
                 <div className="footer">
                     <div className="rating">{rating}</div>
                     <div className="star-dis">
-                        <img src="https://cdn-icons-png.flaticon.com/128/1828/1828906.png" className="str-btn" onClick={this.minusStars}/>
-                        <img src="https://cdn-icons-png.flaticon.com/128/2107/2107957.png" className="stars"></img>
-                        <img src="https://t4.ftcdn.net/jpg/03/22/32/37/240_F_322323723_HJb8d1u2NuI8dMAjvC62TXbSqn63vpI3.jpg" className="str-btn" onClick={this.addStars}/>
+                        <img alt="decrease" 
+                            src="https://cdn-icons-png.flaticon.com/128/1828/1828906.png" 
+                            className="str-btn" 
+                            onClick={() => {this.props.minusStars(this.props.movie)}}/>
+                        <img alt="stars" src="https://cdn-icons-png.flaticon.com/128/2107/2107957.png" className="stars"></img>
+                        <img alt="increase" src="https://t4.ftcdn.net/jpg/03/22/32/37/240_F_322323723_HJb8d1u2NuI8dMAjvC62TXbSqn63vpI3.jpg" 
+                             className="str-btn" 
+                             onClick={() => {this.props.addStars(this.props.movie)}} />
                         <span>{stars}</span>
                     </div>
                     {   //conditional rendering using ternary operators
-                        this.state.fav ? 
-                            <button className="unfavourite-btn" onClick={this.favHandler}>Un-Favourite</button> : 
-                            <button className="favourite-btn" onClick={this.favHandler}>Favourites</button>
+                        fav ? 
+                            <button className="unfavourite-btn" onClick={() => {this.props.favHandler(this.props.movie)}}>Un-Favourite</button> : 
+                            <button className="favourite-btn" onClick={() => {this.props.favHandler(this.props.movie)}}>Favourites</button>
                     }
-                    <button className={this.state.cart? "unfavourite-btn" : "cart-btn"} onClick={this.cartHandler}>
-                        {this.state.cart ? "Remove From Cart" : "Add to cart"}</button>
+                    <button className={cart? "unfavourite-btn" : "cart-btn"} 
+                        onClick={() => {this.props.cartHandler(this.props.movie)}}>
+                        {cart ? "Remove From Cart" : "Add to cart"}</button>
                 </div>
                 </div>
             </div>
