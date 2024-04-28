9aa5d42fcc0651f390944f0e08bffc84ff7e6542
kushagr14 <klakhanpal14@gmail.com>
2024-04-28 17:44:13 +0530

    added styled components

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,56 +1,62 @@
 import { Component } from "react";
+import styled from "styled-components";
+
+const Nav = styled.div`
+    width: 100%;
+    height: 70px;
+    background-color: rgb(70, 68, 224);
+    display: flex;
+    justify-content: space-between;
+    
+`;
+
+const Title= styled.div`
+    font-size: 30;
+    color: #fff;
+    font-weight: 600;
+    font-family: Montserrat, sans-serif;
+    text-transform: uppercase;
+    margin-left: 20;
+`;
+
+const CartContainer = styled.div`
+    position: relative;
+    cursor: pointer;
+`;
+
+const CartIcon = styled.img`
+    width: 50%; 
+    height: auto;
+    marginRight: 20;
+    
+`;
+
+const CartCount = styled.span`
+        background: orange;
+        border-radius: 50%;
+        padding: 4px 8px;
+        position: absolute;
+        right:10;
+        top: -5;
+        fontSize: 12;        
+`;
 
 class Navbar extends Component{
     render(){
         return(
             <>
-                <div style={styles.nav}>
-                    <div style={styles.title}>Movie App</div>
-                    <div style={styles.cartContainer}>
-                        <img alt="cart icon" style={styles.cartIcon} src="https://cdn-icons-png.flaticon.com/128/891/891462.png"/>
-                        <span style={styles.cartCount}>0</span>
-                    </div>
-                </div>
+                <Nav>
+                    <Title>Movie App</Title>
+                    <CartContainer>
+                        <CartIcon alt="cart-img" src="https://cdn-icons-png.flaticon.com/128/891/891462.png"/>
+                        <CartCount>0</CartCount>
+                    </CartContainer>
+                </Nav>
             </>
         )
     }
 }
 
-const styles = {
-    nav: {
-        height: 70,
-        background: "#4267b2",
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        position: "relative"
-    },
-    title:{
-        marginLeft: 20,
-        padding: 10,
-        fontSize: 30,
-        color: "#fff",
-        fontWeight: 600,
-        fontFamily: '"Montserrat", sans-serif',
-        textTransform: "uppercase"
-    },
-    cartContainer: {
-        position: "relative",
-        cursor: "pointer"
-    },
-    cartIcon:{
-        height: 48,
-        marginRight: 20
-    },
-    cartCount:{
-        background: "orange",
-        borderRadius: "50%",
-        padding: "4px 8px",
-        position: "absolute",
-        right:10, 
-        top: -5,
-        fontSize: 12
-    }
-}
+
 
 export default Navbar;
